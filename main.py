ba6283a1eb36212bc8a9bfb7cfebacaa8a55c3b4
Hari <baluoger91@gmail.com>
2021-06-21 23:44:11 +0530

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -7,19 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.core.window import Window
 '''
-a = random.choice([1,2,3,4,5,6])
-print(a)
 
-switch = {
-    1 : "one",
-    2 : "two",
-    3 : "three",
-    4 : "four",
-    5 : "five",
-    6 : "six"
-}
-s = switch.get(a)
-print(s)
 '''
 class MyPage(BoxLayout):
     def __init__(self):
@@ -29,20 +17,24 @@ class MyPage(BoxLayout):
         self.spacing = 20
         self.label = Label(text="Mr-Di-Roll")
         self.image = Image(source='./images/dice-six-faces-five.png')
-        self.button = Button(text="ROLL",background_color=(0,0,1,1))#,on_press=self.roll)
+        self.button = Button(text="ROLL",background_color=(0,0,1,1),on_press=self.roll)
         self.image.keep_ratio = False
         self.image.allow_stretch = False
         self.add_widget(self.label)
         self.add_widget(self.image)
         self.add_widget(self.button)
     
-    #def roll(self,instace):
+    def roll(self,instace):
         #Need to add dice logic
+        a = random.choice([1,2,3,4,5,6])
+        print(a)
+        if a==1:
+            self.image.source = ('./images/dice-six-faces-one.png')
 
 class MrDiRoll(App):
     def build(self):
-        Window.clearcolor = (0,0,0,1)
-        #self.icon = '#'
+        Window.clearcolor = (1,0,0,1)
+        self.icon = './images/icon.png'
         return MyPage()
 
 if __name__ == '__main__':
